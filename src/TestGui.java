fa2f3031a990dd08e087dc549d1b6f8a6eca354f
Dnpypy <denlatypoff@yandex.ru>
2022-03-29 17:02:23 +0300

    add .gitignore

--- src/TestGui.java
+++ src/TestGui.java
@@ -1,7 +1,13 @@
-
+import javax.swing.*;
 
 public class TestGui {
     public static void main(String[] args) {
-        System.out.println("Hello Swing");
+        JFrame jFrame = new JFrame("Test Frame");
+        jFrame.setSize(400, 400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+
+        JButton jButton = new JButton("Test Button");
+        jFrame.add(jButton);
     }
 }
