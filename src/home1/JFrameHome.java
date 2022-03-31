f586120723b38be1300887a9485c1a410c9d7c6b
Dnpypy <denlatypoff@yandex.ru>
2022-03-31 13:11:43 +0300

    Create class JPanelHome, JFrameHome, JButtonHome

--- src/home1/JFrameHome.java
+++ src/home1/JFrameHome.java
@@ -1,4 +1,19 @@
 package home1;
 
-public class JFrameHome {
+import javax.swing.*;
+import java.awt.*;
+
+public class JFrameHome extends JFrame {
+
+    public JFrameHome(String name, int width, int height) {
+        super.setLocationRelativeTo(null);
+        super.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        super.setLayout(new FlowLayout());
+        super.setVisible(true);
+    }
+
+    public JFrameHome(String name, int width, int height, Component comp) {
+        this(name, width, height);
+        super.getContentPane().add(comp);
+    }
 }
