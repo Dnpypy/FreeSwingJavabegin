ebb72ab7b6792b2a12c0f74e4e5d00e2c193aa4d
Dnpypy <denlatypoff@yandex.ru>
2022-04-05 19:05:39 +0300

    update AbsoluteLayoutDemo class and create class Test in package latyoutsdemo

--- src/layoutsdemo/AbsoluteLayoutDemo.java
+++ src/layoutsdemo/AbsoluteLayoutDemo.java
@@ -1,13 +1,15 @@
 package layoutsdemo;
 
 import java.awt.Rectangle;
-import javax.swing.JButton;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 
 public class AbsoluteLayoutDemo extends LayoutDefaultButtons{
     
-    private JPanel panel;
+    //....
+
 
     @Override
     public void useLayout(JPanel panel) {
@@ -17,12 +19,22 @@ public class AbsoluteLayoutDemo extends LayoutDefaultButtons{
     @Override
     public void addButtonsToPanel(final JPanel panel, int buttonCount) {
         
-        this.panel = panel;
+        //....
+
         JButton button1 = new JButton("Button1");
         button1.setSize(300,300);
         button1.setBounds(new Rectangle(150,200,170,20));
         
-     
+        // home4 1 вариант анонимный класс
+        // при нажатии на кнопку 1 отбработается ActionListener()
+        // что будет происходить по нажатию этой кнопки
+        //
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showConfirmDialog(panel, "text message");
+            }
+        });
         
                 
         JButton button2 = new JButton("Button2");
---
+++ src/layoutsdemo/Test.java
@@ -0,0 +1,9 @@
+package layoutsdemo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        AbsoluteLayoutDemo absoluteLayoutDemo = new AbsoluteLayoutDemo();
+
+    }
+}
